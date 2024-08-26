49ca65d7f67f67ac1043d18f2211ee07a377fa09
Anton Romanov <a.romanovich86@gmail.com>
2024-08-26 20:49:42 +0300

    Separete traits for DML operations

--- src/api/endpoints/arch.rs
+++ src/api/endpoints/arch.rs
@@ -1,11 +1,12 @@
 use crate::api::endpoints::ApiResponse;
 use crate::api::errors::AppError;
 use crate::api::state::ApiState;
-use crate::models::arch::ArchRow;
-use crate::models::SingularModel;
+use crate::models::arch::{ArchRow, ArchRowOptId};
+use crate::models::{SingularModel, SelectRow, InsertRow, UpdateRow, DeleteRow, NextId};
 use axum::extract::{Path, State};
 use axum::Json;
 use serde::{Deserialize, Serialize};
+use crate::models::arch as m;
 
 use axum::{
     routing::{delete, get, post, put},
@@ -13,12 +14,15 @@ use axum::{
 };
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
+#[serde(deny_unknown_fields)]
 pub struct ArchNoId {
+    pub id: Option<i64>,
     pub name: String,
     pub description: Option<String>,
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
+#[serde(deny_unknown_fields)]
 pub struct Arch {
     pub id: i64,
     pub name: String,
@@ -40,14 +44,25 @@ impl From<ArchRow> for Arch {
     }
 }
 
+impl From<ArchNoId> for ArchRowOptId {
+    fn from(v: ArchNoId) -> Self {
+        Self {
+            id: v.id,
+            name: v.name,
+            description: v.description,
+        }
+    }
+}
+
 pub async fn get_arch(
     Path(id): Path<i64>,
     State(app): State<ApiState>,
 ) -> Result<ApiResponse<Arch>, AppError> {
     let mut pg = app.settings.pgclient.connect().await?;
-    let p = ArchRow::select(&mut pg.session, id).await?;
+    // let p = ArchRow::select(&mut pg.session, id).await?;
+    let arch = m::Arch::select(&mut pg.session, id).await?;
     let _ = pg.commit().await?;
-    Ok(ApiResponse::Json(p.into()))
+    Ok(ApiResponse::Json(arch.into()))
 }
 
 pub async fn create_arch(
@@ -55,14 +70,9 @@ pub async fn create_arch(
     Json(data): Json<ArchNoId>,
 ) -> Result<ApiResponse<Arch>, AppError> {
     let mut pg = app.settings.pgclient.connect().await?;
-    let model = ArchRow {
-        id: ArchRow::next_id(&mut pg.session).await?,
-        name: data.name,
-        description: data.description,
-    };
-    let p = model.insert(&mut pg.session).await?;
+    let arch = m::Arch::insert(&mut pg.session, data.into()).await?;
     let _ = pg.commit().await?;
-    Ok(ApiResponse::Json(p.into()))
+    Ok(ApiResponse::Json(arch.into()))
 }
 
 pub async fn update_arch(
@@ -72,14 +82,14 @@ pub async fn update_arch(
 ) -> Result<ApiResponse<Arch>, AppError> {
     let mut pg = app.settings.pgclient.connect().await?;
     dbg!(&data);
-    let model = ArchRow {
+    let data = ArchRow {
         id,
         name: data.name,
         description: data.description,
     };
-    let p = model.update(&mut pg.session).await?;
+    let arch = m::Arch::update(&mut pg.session, data).await?;
     let _ = pg.commit().await?;
-    Ok(ApiResponse::Json(p.into()))
+    Ok(ApiResponse::Json(arch.into()))
 }
 
 pub async fn delete_arch(
@@ -87,7 +97,7 @@ pub async fn delete_arch(
     State(app): State<ApiState>,
 ) -> Result<ApiResponse<Id>, AppError> {
     let mut pg = app.settings.pgclient.connect().await?;
-    let p = ArchRow::delete(&mut pg.session, id).await?;
+    let id = m::Arch::delete(&mut pg.session, id).await?;
     let _ = pg.commit().await?;
     Ok(ApiResponse::Json(Id { id: id }))
 }
--- src/models.rs
+++ src/models.rs
@@ -39,3 +39,67 @@ where
 // }
 
 pub mod arch;
+
+pub(crate) trait Model {
+    const name: &'static str;
+    type Id;
+}
+
+pub(crate) trait SelectRow<'a>
+where
+    Self: Sized,
+{
+    type Id;
+    type Error;
+    type Row;
+    type Session;
+
+    async fn select(s: &mut Self::Session, id: Self::Id) -> Result<Self::Row, Self::Error>;
+}
+
+pub(crate) trait InsertRow<'a>
+where
+    Self: Sized,
+{
+    type Id;
+    type Error;
+    type Row;
+    type Session;
+    type Data;
+
+    async fn insert(s: &mut Self::Session, data: Self::Data) -> Result<Self::Row, Self::Error>;
+}
+
+pub(crate) trait UpdateRow<'a>
+where
+    Self: Sized,
+{
+    type Id;
+    type Error;
+    type Row;
+    type Session;
+
+    async fn update(s: &mut Self::Session, data: Self::Row) -> Result<Self::Row, Self::Error>;
+}
+
+pub(crate) trait DeleteRow<'a>
+where
+    Self: Sized,
+{
+    type Id;
+    type Error;
+    type Session;
+
+    async fn delete(s: &mut Self::Session, id: Self::Id) -> Result<Self::Id, Self::Error>;
+}
+
+pub(crate) trait NextId<'a>
+where
+    Self: Sized,
+{
+    type Id;
+    type Error;
+    type Session;
+
+    async fn next_id(s: &mut Self::Session) -> Result<Self::Id, Self::Error>;
+}
